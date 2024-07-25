4ba7038aeffda9abf6edf3d7d9defc9a4809b81c
Nikita <perfect1201@ukr.net>
2024-07-26 01:31:17 +0300

    deleted jsx fragment in Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,12 +5,10 @@ export const Person = ({ person }) => {
   const { name, age } = person;
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">My name is {name}</h2>
-        {age !== undefined && <p className="Person__age">I am {age}</p>}
-        <Partner person={person} />
-      </section>
-    </>
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <Partner person={person} />
+    </section>
   );
 };
